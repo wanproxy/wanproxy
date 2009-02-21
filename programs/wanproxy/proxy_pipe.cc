3ebb490576e442e8d19b61440dfe2e7680267adc
Juli Mallett <juli@clockworksquid.com>
2009-02-21 10:19:13 +0000

    Start work on a ProxyPipe class, not hooked up to anything, to replace the ugly hack that is ProxyPeer.  ProxyClient will instead work in terms of a pair of pipes, I believe.

---
+++ programs/wanproxy/proxy_pipe.cc
@@ -0,0 +1,222 @@
+#include <common/buffer.h>
+#include <common/endian.h>
+
+#include <event/action.h>
+#include <event/callback.h>
+#include <event/event_system.h>
+
+#include <io/channel.h>
+#include <io/file_descriptor.h>
+
+#include <xcodec/xchash.h>
+#include <xcodec/xcodec.h>
+#include <xcodec/xcodec_decoder.h>
+#include <xcodec/xcodec_encoder.h>
+
+#include "proxy_pipe.h"
+
+ProxyPipe::ProxyPipe(XCodec *read_codec, Channel *read_channel,
+		     Channel *write_channel, XCodec *write_codec)
+: log_("/wanproxy/proxy/pipe"),
+  read_action_(NULL),
+  read_buffer_(),
+  read_channel_(read_channel),
+  read_decoder_(NULL),
+  write_action_(NULL),
+  write_buffer_(),
+  write_channel_(write_channel),
+  write_encoder_(NULL),
+  flow_action_(NULL),
+  flow_callback_(NULL)
+{
+	if (read_codec != NULL)
+		read_decoder_ = read_codec->decoder();
+	if (write_codec != NULL)
+		write_encoder_ = write_codec->encoder();
+}
+
+ProxyPipe::~ProxyPipe()
+{
+	ASSERT(read_action_ == NULL);
+	ASSERT(write_action_ == NULL);
+
+	if (read_decoder_ != NULL) {
+		delete read_decoder_;
+		read_decoder_ = NULL;
+	}
+
+	if (write_encoder_ != NULL) {
+		delete write_encoder_;
+		write_encoder_ = NULL;
+	}
+
+	ASSERT(flow_action_ == NULL);
+}
+
+Action *
+ProxyPipe::flow(void)
+{
+	ASSERT(flow_callback_ == NULL);
+
+	schedule_read();
+	return (cancellation(this, &ProxyPipe::flow_cancel));
+}
+
+void
+ProxyPipe::read_complete(Event e, void *)
+{
+	read_action_->cancel();
+	read_action_ = NULL;
+
+	switch (e.type_) {
+	case Event::Done:
+	case Event::EOS:
+		break;
+	default:
+		ERROR(log_) << "Unexpected event: " << e;
+		read_error();
+		return;
+	}
+
+	read_buffer_.append(e.buffer_);
+	if (!read_buffer_.empty()) {
+		Buffer output;
+
+		if (read_decoder_ != NULL) {
+			if (!read_decoder_->decode(&output, &read_buffer_)) {
+				read_error();
+				return;
+			}
+		} else {
+			output = read_buffer_;
+			read_buffer_.clear();
+		}
+
+		if (write_encoder_ != NULL) {
+			write_encoder_->encode(&write_buffer_, &output);
+		} else {
+			write_buffer_.append(output);
+		}
+
+		schedule_write();
+	}
+
+	if (e.type_ == Event::EOS)
+		read_channel_ = NULL;
+	else
+		schedule_read();
+}
+
+void
+ProxyPipe::write_complete(Event e, void *)
+{
+	write_action_->cancel();
+	write_action_ = NULL;
+
+	switch (e.type_) {
+	case Event::Done:
+		break;
+	default:
+		ERROR(log_) << "Unexpected event: " << e;
+		write_error();
+		return;
+	}
+
+	/*
+	 * If the read channel is gone (due to EOS) and we've finished our
+	 * writing, then tear down this flow.
+	 */
+	if (write_buffer_.empty() && read_channel_ == NULL) {
+		flow_close();
+	} else {
+		schedule_write();
+	}
+}
+
+void
+ProxyPipe::schedule_read(void)
+{
+	ASSERT(read_action_ == NULL);
+	ASSERT(read_channel_ != NULL);
+
+	EventCallback *cb = callback(this, &ProxyPipe::read_complete);
+	read_action_ = read_channel_->read(cb);
+}
+
+void
+ProxyPipe::schedule_write(void)
+{
+	if (write_action_ == NULL)
+		return;
+
+	if (write_buffer_.empty())
+		return;
+
+	EventCallback *cb = callback(this, &ProxyPipe::write_complete);
+	write_action_ = write_channel_->write(&write_buffer_, cb);
+}
+
+void
+ProxyPipe::read_error(void)
+{
+	ASSERT(read_action_ == NULL);
+
+	if (write_action_ != NULL) {
+		write_action_->cancel();
+		write_action_ = NULL;
+	}
+
+	flow_error();
+}
+
+void
+ProxyPipe::write_error(void)
+{
+	ASSERT(write_action_ == NULL);
+
+	if (read_action_ != NULL) {
+		read_action_->cancel();
+		read_action_ = NULL;
+	}
+
+	flow_error();
+}
+
+void
+ProxyPipe::flow_close(void)
+{
+	ASSERT(flow_action_ == NULL);
+	ASSERT(flow_callback_ != NULL);
+
+	flow_callback_->event(Event(Event::Done, 0));
+	Action *a = EventSystem::instance()->schedule(flow_callback_);
+	flow_action_ = a;
+	flow_callback_ = NULL;
+}
+
+void
+ProxyPipe::flow_error(void)
+{
+	ASSERT(flow_action_ == NULL);
+	ASSERT(flow_callback_ != NULL);
+
+	/* XXX Preserve error code.  */
+	flow_callback_->event(Event(Event::Error, 0));
+	Action *a = EventSystem::instance()->schedule(flow_callback_);
+	flow_action_ = a;
+	flow_callback_ = NULL;
+}
+
+void
+ProxyPipe::flow_cancel(void)
+{
+	if (flow_action_ != NULL) {
+		flow_action_->cancel();
+		flow_action_ = NULL;
+	}
+
+	if (flow_callback_ != NULL) {
+		delete flow_callback_;
+		flow_callback_ = NULL;
+	}
+}
---
+++ programs/wanproxy/proxy_pipe.h
@@ -0,0 +1,43 @@
+#ifndef	PROXY_PIPE_H
+#define	PROXY_PIPE_H
+
+class XCodecDecoder;
+class XCodecEncoder;
+
+class ProxyPipe {
+	LogHandle log_;
+
+	Action *read_action_;
+	Buffer read_buffer_;
+	Channel *read_channel_;
+	XCodecDecoder *read_decoder_;
+
+	Action *write_action_;
+	Buffer write_buffer_;
+	Channel *write_channel_;
+	XCodecEncoder *write_encoder_;
+
+	Action *flow_action_;
+	EventCallback *flow_callback_;
+public:
+	ProxyPipe(XCodec *, Channel *, Channel *, XCodec *);
+	~ProxyPipe();
+
+	Action *flow(void);
+
+private:
+	void read_complete(Event, void *);
+	void write_complete(Event, void *);
+
+	void schedule_read(void);
+	void schedule_write(void);
+
+	void read_error(void);
+	void write_error(void);
+
+	void flow_close(void);
+	void flow_error(void);
+	void flow_cancel(void);
+};
+
+#endif /* !PROXY_PIPE_H */
