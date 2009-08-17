5457366587416b7d418eb8bc576f65d09b2c1c09
Juli Mallett <juli@clockworksquid.com>
2009-08-17 10:48:05 +0000

    Only build address class and address family type if the io module is being compiled in.

--- config/lib.mk
+++ config/lib.mk
@@ -6,11 +6,14 @@
 
 SRCS+=	config.cc
 SRCS+=	config_class.cc
-SRCS+=	config_class_address.cc
 SRCS+=	config_class_log_mask.cc
 SRCS+=	config_object.cc
-SRCS+=	config_type_address_family.cc
 SRCS+=	config_type_int.cc
 SRCS+=	config_type_log_level.cc
 SRCS+=	config_type_pointer.cc
 SRCS+=	config_type_string.cc
+
+.if ${USE_LIBS:Mio}
+SRCS+=	config_class_address.cc
+SRCS+=	config_type_address_family.cc
+.endif
