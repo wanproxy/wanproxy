d9ede3faf3fd9a32bb2dfa9ab511e3fec06063c5
Juli Mallett <juli@clockworksquid.com>
2009-09-05 21:25:03 +0000

    Reluctantly define uintptr_t on Interix.

--- common/types.h
+++ common/types.h
@@ -8,6 +8,7 @@
 #if defined(__OPENNT)
 typedef	unsigned long long	uintmax_t;
 typedef	long long		intmax_t;
+typedef	unsigned int		uintptr_t; /* XXX Appears to be in stddef.h!  */
 #endif
 
 #endif /* !COMMON_TYPES_H */
