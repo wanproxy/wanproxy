d302d66db94a046c05473890cb804b92c1ead74b
Juli Mallett <juli@clockworksquid.com>
2009-09-05 21:56:28 +0000

    Add more types for Interix.

--- common/types.h
+++ common/types.h
@@ -9,6 +9,8 @@
 typedef	unsigned long long	uintmax_t;
 typedef	long long		intmax_t;
 typedef	unsigned int		uintptr_t; /* XXX Appears to be in stddef.h!  */
+typedef	int			socklen_t;
+typedef	unsigned long long	uint64_t;
 #endif
 
 #endif /* !COMMON_TYPES_H */
