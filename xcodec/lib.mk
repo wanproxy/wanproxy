10a8480de139965fa24a299f890ca796c7b78cf3
Juli Mallett <juli@clockworksquid.com>
2009-09-10 13:07:02 +0000

    Don't include pipe-related code if not using the io library.

--- xcodec/lib.mk
+++ xcodec/lib.mk
@@ -6,6 +6,9 @@
 
 SRCS+=	xcodec.cc
 SRCS+=	xcodec_decoder.cc
-SRCS+=	xcodec_decoder_pipe.cc
 SRCS+=	xcodec_encoder.cc
+
+.if ${USE_LIBS:Mio}
+SRCS+=	xcodec_decoder_pipe.cc
 SRCS+=	xcodec_encoder_pipe.cc
+.endif
