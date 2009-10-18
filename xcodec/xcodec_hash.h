d1e0c5ba200847f2e5024034ef1aa0aa046e7ad6
Juli Mallett <juli@clockworksquid.com>
2009-10-18 06:14:37 +0000

    Remove magical-thinking non-entropy from the hash.  More collisions, but faster and not so much worse.

--- xcodec/xcodec_hash.h
+++ xcodec/xcodec_hash.h
@@ -6,19 +6,11 @@
 template<unsigned Tlength>
 class XCodecHash {
 	Adler64<Tlength> adler64_;
-	uint8_t xor_;
-	uint8_t buffer_[Tlength];
-	uint8_t start_;
 
 public:
 	XCodecHash(void)
-	: adler64_(),
-	  xor_(0),
-	  buffer_(),
-	  start_(0)
-	{
-		memset(buffer_, 0, Tlength);
-	}
+	: adler64_()
+	{ }
 
 	~XCodecHash()
 	{ }
@@ -26,17 +18,6 @@ public:
 	void roll(uint8_t ch)
 	{
 		adler64_ += ch;
-
-		uint8_t dead;
-
-		dead = buffer_[start_];
-
-		xor_ ^= dead;
-
-		buffer_[start_] = ch;
-		start_ = (start_ + 1) % Tlength;
-
-		xor_ ^= ch;
 	}
 
 	static uint32_t mix(uint32_t a, uint32_t b, uint32_t c)
@@ -84,21 +65,13 @@ public:
 	}
 
 	struct mix_functor {
-		uint8_t c_;
-		uint8_t x_;
-
-		mix_functor(const uint8_t& c, const uint8_t& x)
-		: c_(c),
-		  x_(x)
-		{ }
-
 		uintmax_t operator() (const uint32_t& a, const uint32_t& b)
 		{
 			uint64_t sum;
 
 			sum = 0;
-			sum |= mix(a, b, (b - a) + c_);
-			sum |= (uint64_t)mix(Tlength, c_ << 8 | x_, (uint32_t)sum) << 32;
+			sum |= mix(a, b, b - a);
+			sum |= (uint64_t)mix(Tlength, b ^ a, (uint32_t)sum) << 32;
 
 			return (sum);
 		}
@@ -106,7 +79,7 @@ public:
 
 	uint64_t mix(void) const
 	{
-		mix_functor f(buffer_[start_], xor_);
+		mix_functor f;
 
 		return (adler64_.mix(f));
 	}
