06087dd655f9101f46c4955f817034c80018bfc9
Juli Mallett <juli@clockworksquid.com>
2010-05-31 10:39:13 +0000

    Add a simple example of reloading.

---
+++ event/example/reload-test1/reload-test1.cc
@@ -0,0 +1,52 @@
+#include <common/buffer.h> /* XXX For class Event */
+
+#include <event/action.h>
+#include <event/callback.h>
+#include <event/event_system.h>
+
+class ReloadTest {
+	Action *action_;
+	Action *reload_action_;
+	unsigned reload_count_;
+public:
+	ReloadTest(void)
+	: action_(NULL),
+	  reload_action_(NULL),
+	  reload_count_(0)
+	{
+		action_ = EventSystem::instance()->schedule(callback(this, &ReloadTest::forever));
+		reload_action_ = EventSystem::instance()->register_interest(EventInterestReload, callback(this, &ReloadTest::reload));
+	}
+
+	~ReloadTest()
+	{
+		ASSERT(action_ == NULL);
+	}
+
+private:
+	void forever(void)
+	{
+		action_->cancel();
+		action_ = NULL;
+
+		action_ = EventSystem::instance()->schedule(callback(this, &ReloadTest::forever));
+	}
+
+	void reload(void)
+	{
+		reload_action_->cancel();
+		reload_action_ = NULL;
+
+		INFO("/event/reload/test1") << "Reload #" << ++reload_count_;
+
+		reload_action_ = EventSystem::instance()->register_interest(EventInterestReload, callback(this, &ReloadTest::reload));
+	}
+};
+
+int
+main(void)
+{
+	ReloadTest *tt = new ReloadTest();
+	EventSystem::instance()->start();
+	delete tt;
+}
