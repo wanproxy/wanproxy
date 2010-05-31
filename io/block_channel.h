7e2e24debd282664431f07d6142f2d9aad4ee41a
Juli Mallett <juli@clockworksquid.com>
2010-05-31 08:35:38 +0000

    Sketch a possible interface for a block-oriented Channel.

---
+++ io/block_channel.h
@@ -0,0 +1,22 @@
+#ifndef	BLOCK_CHANNEL_H
+#define	BLOCK_CHANNEL_H
+
+class Action;
+class Buffer;
+class EventCallback;
+
+class BlockChannel {
+protected:
+	BlockChannel(void)
+	{ }
+
+public:
+	virtual ~BlockChannel()
+	{ }
+
+	virtual Action *close(EventCallback *) = 0;
+	virtual Action *read(off_t, EventCallback *) = 0;
+	virtual Action *write(off_t, Buffer *, EventCallback *) = 0;
+};
+
+#endif /* !BLOCK_CHANNEL_H */
