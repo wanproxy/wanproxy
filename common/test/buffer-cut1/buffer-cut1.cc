2e484e90b47ba9744183cd2fd0a27916999496f3
Juli Mallett <juli@clockworksquid.com>
2010-11-17 12:28:30 +0000

    Use lots of different fill lengths.

--- common/test/buffer-cut1/buffer-cut1.cc
+++ common/test/buffer-cut1/buffer-cut1.cc
@@ -10,53 +10,56 @@ main(void)
 {
 	TestGroup g("/test/buffer/cut1", "Buffer::cut #1");
 
-	Buffer big;
+	unsigned n;
+	for (n = 1; n <= FILL_LENGTH; n++) {
+		Buffer big;
 
-	big.append("Hello, ");
-	size_t headerlen = big.length();
+		big.append("Hello, ");
+		size_t headerlen = big.length();
 
-	uint8_t fill[FILL_LENGTH];
-	unsigned i;
-	for (i = 0; i < sizeof fill; i++) {
-		fill[i] = FILL_BYTE;
-	}
-
-	for (i = 0; i < FILL_NUMBER; i++) {
-		big.append(fill, sizeof fill);
-	}
-
-	big.append("world!\n");
-
-	{
-		Test _(g, "Cut fill bytes.");
-
-		Buffer small(big);
-		small.cut(headerlen, FILL_NUMBER * FILL_LENGTH);
-		if (small.equal("Hello, world!\n"))
-			_.pass();
-	}
-
-	{
-		Test _(g, "Proxy skip and trim.");
-
-		big.cut(0, headerlen);
-		big.cut(FILL_NUMBER * FILL_LENGTH, big.length() - (FILL_NUMBER * FILL_LENGTH));
-		if (big.length() == FILL_NUMBER * FILL_LENGTH)
-			_.pass();
-	}
-
-	{
-		Test _(g, "Correct fill byte checksum.");
-
-		uint64_t sum = 0;
-		while (!big.empty()) {
-			sum += big.peek();
-			big.skip(1);
+		uint8_t fill[n];
+		unsigned i;
+		for (i = 0; i < sizeof fill; i++) {
+			fill[i] = FILL_BYTE;
 		}
 
-		sum /= FILL_LENGTH * FILL_NUMBER * FILL_BYTE;
-		if (sum == 1)
-			_.pass();
+		for (i = 0; i < FILL_NUMBER; i++) {
+			big.append(fill, sizeof fill);
+		}
+
+		big.append("world!\n");
+
+		{
+			Test _(g, "Cut fill bytes.");
+
+			Buffer small(big);
+			small.cut(headerlen, FILL_NUMBER * n);
+			if (small.equal("Hello, world!\n"))
+				_.pass();
+		}
+
+		{
+			Test _(g, "Proxy skip and trim.");
+
+			big.cut(0, headerlen);
+			big.cut(FILL_NUMBER * n, big.length() - (FILL_NUMBER * n));
+			if (big.length() == FILL_NUMBER * n)
+				_.pass();
+		}
+
+		{
+			Test _(g, "Correct fill byte checksum.");
+
+			uint64_t sum = 0;
+			while (!big.empty()) {
+				sum += big.peek();
+				big.skip(1);
+			}
+
+			sum /= FILL_NUMBER * n * FILL_BYTE;
+			if (sum == 1)
+				_.pass();
+		}
 	}
 
 	return (0);
