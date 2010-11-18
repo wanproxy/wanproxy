cac910f5c0e6c708f46ed80ca8ceb0b49483fdce
Juli Mallett <juli@clockworksquid.com>
2010-11-18 21:11:45 +0000

    Add a source of zeroes.

---
+++ io/example/zero-source1/zero-source1.cc
@@ -0,0 +1,86 @@
+#include <unistd.h>
+
+#include <common/buffer.h>
+
+#include <event/action.h>
+#include <event/callback.h>
+#include <event/event.h>
+#include <event/event_system.h>
+
+#include <io/file_descriptor.h>
+
+static Buffer zero_buffer;
+
+class Source {
+	LogHandle log_;
+
+	FileDescriptor fd_;
+	Action *action_;
+public:
+	Source(int fd)
+	: log_("/source"),
+	  fd_(fd),
+	  action_(NULL)
+	{
+		Buffer tmp(zero_buffer);
+		EventCallback *cb = callback(this, &Source::write_complete);
+		action_ = fd_.write(&tmp, cb);
+	}
+
+	~Source()
+	{
+		ASSERT(action_ == NULL);
+	}
+
+	void write_complete(Event e)
+	{
+		action_->cancel();
+		action_ = NULL;
+
+		switch (e.type_) {
+		case Event::Done:
+		case Event::Error:
+			break;
+		default:
+			HALT(log_) << "Unexpected event: " << e;
+			return;
+		}
+
+		if (e.type_ == Event::Error) {
+			EventCallback *cb = callback(this, &Source::close_complete);
+			action_ = fd_.close(cb);
+			return;
+		}
+
+		Buffer tmp(zero_buffer);
+		EventCallback *cb = callback(this, &Source::write_complete);
+		action_ = fd_.write(&tmp, cb);
+	}
+
+	void close_complete(Event e)
+	{
+		action_->cancel();
+		action_ = NULL;
+
+		switch (e.type_) {
+		case Event::Done:
+			break;
+		default:
+			HALT(log_) << "Unexpected event: " << e;
+			return;
+		}
+	}
+};
+
+int
+main(void)
+{
+	static uint8_t zbuf[8192];
+	memset(zbuf, 0, sizeof zbuf);
+
+	zero_buffer.append(zbuf, sizeof zbuf);
+
+	Source source(STDOUT_FILENO);
+
+	EventSystem::instance()->start();
+}
