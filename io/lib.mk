24bfed009420fa1ace0fadb31c86e389369bf4f8
Juli Mallett <juli@clockworksquid.com>
2010-12-05 09:32:27 +0000

    Use libnetwork on Haiku.

--- io/lib.mk
+++ io/lib.mk
@@ -14,6 +14,11 @@ SRCS+=	splice_pair.cc
 SRCS+=	unix_client.cc
 SRCS+=	unix_server.cc
 
+ifeq "${OSNAME}" "Haiku"
+# Required for sockets.
+LDADD+=		-lnetwork
+endif
+
 ifeq "${OSNAME}" "SunOS"
 # Required for sockets.
 LDADD+=		-lnsl
