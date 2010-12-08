fbaee438d4cfb8fdb97cf8e302ce81eb912cdfcc
Juli Mallett <juli@clockworksquid.com>
2010-12-08 03:25:32 +0000

    Merge Splice use from DeflatePipe.

--- zlib/example/inflate-pipe1/inflate-pipe1.cc
+++ zlib/example/inflate-pipe1/inflate-pipe1.cc
@@ -9,6 +9,7 @@
 
 #include <io/file_descriptor.h>
 #include <io/pipe.h>
+#include <io/splice.h>
 
 #include <zlib/inflate_pipe.h>
 
@@ -18,36 +19,37 @@ class Catenate {
 	FileDescriptor input_;
 	Action *input_action_;
 
-	Pipe *pipe_;
-
 	FileDescriptor output_;
 	Action *output_action_;
+
+	Splice splice_;
+	Action *splice_action_;
 public:
 	Catenate(int input, Pipe *pipe, int output)
 	: log_("/catenate"),
 	  input_(input),
 	  input_action_(NULL),
-	  pipe_(pipe),
 	  output_(output),
-	  output_action_(NULL)
+	  output_action_(NULL),
+	  splice_(&input_, pipe, &output_)
 	{
-		EventCallback *cb = callback(this, &Catenate::read_complete);
-		input_action_ = input_.read(0, cb);
+		EventCallback *cb = callback(this, &Catenate::splice_complete);
+		splice_action_ = splice_.start(cb);
 	}
 
 	~Catenate()
 	{
 		ASSERT(input_action_ == NULL);
 		ASSERT(output_action_ == NULL);
+		ASSERT(splice_action_ == NULL);
 	}
 
-	void read_complete(Event e)
+	void splice_complete(Event e)
 	{
-		input_action_->cancel();
-		input_action_ = NULL;
+		splice_action_->cancel();
+		splice_action_ = NULL;
 
 		switch (e.type_) {
-		case Event::Done:
 		case Event::EOS:
 			break;
 		default:
@@ -55,70 +57,11 @@ public:
 			return;
 		}
 
-		EventCallback *cb = callback(this, &Catenate::input_complete);
-		input_action_ = pipe_->input(&e.buffer_, cb);
-	}
+		EventCallback *icb = callback(this, &Catenate::close_complete, &input_);
+		input_action_ = input_.close(icb);
 
-	void input_complete(Event e)
-	{
-		input_action_->cancel();
-		input_action_ = NULL;
-
-		switch (e.type_) {
-		case Event::Done:
-			break;
-		default:
-			HALT(log_) << "Unexpected event: " << e;
-			return;
-		}
-
-		EventCallback *cb = callback(this, &Catenate::output_complete);
-		output_action_ = pipe_->output(cb);
-	}
-
-	void output_complete(Event e)
-	{
-		output_action_->cancel();
-		output_action_ = NULL;
-
-		switch (e.type_) {
-		case Event::Done:
-		case Event::EOS:
-			break;
-		default:
-			HALT(log_) << "Unexpected event: " << e;
-			return;
-		}
-
-		if (!e.buffer_.empty()) {
-			EventCallback *cb = callback(this, &Catenate::write_complete);
-			output_action_ = output_.write(&e.buffer_, cb);
-		} else {
-			ASSERT(e.type_ == Event::EOS);
-
-			EventCallback *icb = callback(this, &Catenate::close_complete, &input_);
-			input_action_ = input_.close(icb);
-
-			EventCallback *ocb = callback(this, &Catenate::close_complete, &output_);
-			output_action_ = output_.close(ocb);
-		}
-	}
-
-	void write_complete(Event e)
-	{
-		output_action_->cancel();
-		output_action_ = NULL;
-
-		switch (e.type_) {
-		case Event::Done:
-			break;
-		default:
-			HALT(log_) << "Unexpected event: " << e;
-			return;
-		}
-
-		EventCallback *cb = callback(this, &Catenate::read_complete);
-		input_action_ = input_.read(0, cb);
+		EventCallback *ocb = callback(this, &Catenate::close_complete, &output_);
+		output_action_ = output_.close(ocb);
 	}
 
 	void close_complete(Event e, FileDescriptor *fd)
