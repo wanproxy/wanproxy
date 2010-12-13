87b1985f811119949a80b638c44897469058e835
Juli Mallett <juli@clockworksquid.com>
2010-12-13 03:31:58 +0000

    Use a condition variable rather than spinning.

--- common/thread/mutex_posix.h
+++ common/thread/mutex_posix.h
@@ -4,6 +4,7 @@
 struct MutexState {
 	pthread_mutex_t mutex_;
 	pthread_mutexattr_t mutex_attr_;
+	pthread_cond_t cond_;
 	Thread *owner_;
 
 	MutexState(void)
@@ -21,6 +22,8 @@ struct MutexState {
 
 		rv = pthread_mutex_init(&mutex_, &mutex_attr_);
 		ASSERT(rv != -1);
+
+		rv = pthread_cond_init(&cond_, NULL);
 	}
 
 	~MutexState()
@@ -31,6 +34,9 @@ struct MutexState {
 		ASSERT(rv != -1);
 
 		rv = pthread_mutexattr_destroy(&mutex_attr_);
+		ASSERT(rv != -1);
+
+		rv = pthread_cond_destroy(&cond_);
 		ASSERT(rv != -1);
 
 #if 0 /* XXX What about extern Mutexes?  */
@@ -59,11 +65,8 @@ struct MutexState {
 		Thread *self = Thread::self();
 		ASSERT(self != NULL);
 
-		while (owner_ != NULL) {
-			unlock();
-			/* XXX Wait on a condvar instead?  */
-			lock();
-		}
+		while (owner_ != NULL)
+			pthread_cond_wait(&cond_, &mutex_);
 		owner_ = self;
 	}
 
@@ -92,6 +95,8 @@ struct MutexState {
 		}
 		ASSERT(owner_ == self);
 		owner_ = NULL;
+
+		pthread_cond_signal(&cond_);
 	}
 };
 
