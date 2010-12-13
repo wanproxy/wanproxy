a3030c8ef0073fe323f93974285b897b10a0e31b
Juli Mallett <juli@clockworksquid.com>
2010-12-13 08:46:53 +0000

    Use many more threads.

--- common/thread/test/thread-main1/thread-main1.cc
+++ common/thread/test/thread-main1/thread-main1.cc
@@ -2,11 +2,14 @@
 
 #include <common/thread/thread.h>
 
+#define	NTHREAD		8
+#define	ROUNDS		1024
+
 class TestThread : public Thread {
-	Test& test_main_;
-	Test& test_destroy_;
+	Test *test_main_;
+	Test *test_destroy_;
 public:
-	TestThread(Test& test_main, Test& test_destroy)
+	TestThread(Test *test_main, Test *test_destroy)
 	: Thread("TestThread"),
 	  test_main_(test_main),
 	  test_destroy_(test_destroy)
@@ -14,29 +17,47 @@ public:
 
 	~TestThread()
 	{
-		test_destroy_.pass();
+		test_destroy_->pass();
 	}
 
 	void main(void)
 	{
-		test_main_.pass();
+		test_main_->pass();
 	}
 };
 
 int
 main(void)
 {
+	Thread *threads[NTHREAD];
+	Test *test_main[NTHREAD], *test_destroy[NTHREAD];
+
 	TestGroup g("/test/thread/main1", "Thread::main #1");
 
-	Test test_main(g, "Main function called.");
-	Test test_destroy(g, "Destructor called.");
-	Thread *td = new TestThread(test_main, test_destroy);
+	unsigned j;
+	for (j = 0; j < ROUNDS; j++) {
+		unsigned i;
+		for (i = 0; i < NTHREAD; i++) {
+			test_main[i] = new Test(g, "Main function called.");
+			test_destroy[i] = new Test(g, "Destructor called.");
+			threads[i] = new TestThread(test_main[i], test_destroy[i]);
+		}
 
-	td->start();
+		for (i = 0; i < NTHREAD; i++)
+			threads[i]->start();
 
-	td->join();
+		for (i = 0; i < NTHREAD; i++)
+			threads[i]->join();
 
-	delete td;
+		for (i = 0; i < NTHREAD; i++)
+			delete test_main[i];
+
+		for (i = 0; i < NTHREAD; i++)
+			delete threads[i];
+
+		for (i = 0; i < NTHREAD; i++)
+			delete test_destroy[i];
+	}
 
 	return (0);
 }
