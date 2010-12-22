5cfdbed3fb336777802c0eb9385cd0efed5d2bfd
Juli Mallett <juli@clockworksquid.com>
2010-12-22 09:59:28 +0000

    Start sketching what an XCodecSymbol class might look like.

---
+++ xcodec/xcodec_symbol.h
@@ -0,0 +1,41 @@
+#ifndef	XCODEC_SYMBOL_H
+#define	XCODEC_SYMBOL_H
+
+union XCodecSymbol {
+	struct {
+		uint64_t reserved_:3;
+		uint64_t hash_:61;
+	} fields_;
+	uint64_t word_;
+
+	XCodecSymbol(void)
+	: word_(0)
+	{ }
+
+	void append(Buffer *buf)
+	{
+		uint64_t word = BigEndian::encode(word_);
+
+		buf->append(&word);
+	}
+
+	void extract(Buffer *buf, unsigned offset = 0)
+	{
+		ASSERT(buf->length() >= sizeof word_ + offset);
+
+		uint64_t word;
+
+		buf->extract(&word, offset);
+		word_ = BigEndian::decode(word);
+	}
+
+	void moveout(Buffer *buf, unsigned offset = 0)
+	{
+		ASSERT(buf->length() >= sizeof word_ + offset);
+		if (offset != 0)
+			buf->skip(offset);
+		buf->extract(buf);
+	}
+};
+
+#endif /* !XCODEC_SYMBOL_H */
