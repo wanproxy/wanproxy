b89f68bbb56257af0dafa79e1755db578a89b629
Juli Mallett <juli@clockworksquid.com>
2011-07-25 05:14:11 +0000

    Fix ~Lion~ build.

--- crypto/lib.mk
+++ crypto/lib.mk
@@ -2,4 +2,9 @@ VPATH+=	${TOPDIR}/crypto
 
 SRCS+=	crypto_encryption_openssl.cc
 
+# Apple has decided to deprecate all of OpenSSL in Mac OS X ~Lion~.  Brilliant.
+ifeq "${OSNAME}" "Darwin"
+CFLAGS+=	-Wno-deprecated-declarations
+endif
+
 LDADD+=		-lcrypto
