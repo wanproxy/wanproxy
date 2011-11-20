99a7f109df3be1d9e8c1a80c725e97fe0dc8dc8a
Juli Mallett <juli@clockworksquid.com>
2011-11-20 09:12:13 +0000

    Follow pointers and allow selection of a single object.

--- programs/wanproxy/monitor_client.cc
+++ programs/wanproxy/monitor_client.cc
@@ -5,6 +5,7 @@
 #include <config/config_exporter.h>
 #include <config/config_object.h>
 #include <config/config_type.h>
+#include <config/config_type_pointer.h>
 
 #include <event/action.h>
 #include <event/callback.h>
@@ -19,11 +20,13 @@
 #include "monitor_client.h"
 
 class HTMLConfigExporter : public ConfigExporter {
+	std::string select_;
 public:
 	std::ostringstream os_;
 
-	HTMLConfigExporter(void)
-	: os_()
+	HTMLConfigExporter(const std::string& select)
+	: select_(select),
+	  os_()
 	{ }
 
 	~HTMLConfigExporter()
@@ -31,7 +34,10 @@ public:
 
 	void config(const Config *c)
 	{
-		os_ << "<h1>Configuration</h1>";
+		if (select_ == "")
+			os_ << "<h1>Configuration</h1>";
+		else
+			os_ << "<h1>Configuration - " + select_ + "</h1>";
 		c->marshall(this);
 		os_ << "<hr />";
 	}
@@ -48,16 +54,29 @@ public:
 
 	void object(const ConfigClass *cc, const ConfigObject *co)
 	{
+		if (select_ != "" && select_ != co->name())
+			return;
 		os_ << "<table>";
-		os_ << "<tr><th>object</th><th>name</th><th>class</th><th>value</th></tr>";
-		os_ << "<tr><td>" << co->name() << "</td><td>" << (void *)co << "</td><td>" << cc->name() << "</td><td>";
+		os_ << "<tr><th>object</th><th>class</th><th>value</th></tr>";
+		os_ << "<tr><td><a href=\"/" << co->name() << "\">" << co->name() << "</a></td><td>" << cc->name() << "</td><td>";
 		cc->marshall(this, co);
 		os_ << "</td></tr>";
 		os_ << "</table>";
 	}
 
-	void value(const ConfigValue *, const std::string& val)
+	void value(const ConfigValue *cv, const std::string& val)
 	{
+		ConfigTypePointer *ct = dynamic_cast<ConfigTypePointer *>(cv->type_);
+		if (ct != NULL) {
+			ConfigObject *co;
+			if (ct->get(cv, &co)) {
+				if (co == NULL)
+					os_ << "<tt>None</tt>";
+				else
+					os_ << "<a href=\"/" << co->name() << "\">" << co->name() << "</a>";
+				return;
+			}
+		}
 		os_ << "<tt>" << val << "</tt>";
 	}
 };
@@ -71,12 +90,22 @@ MonitorClient::handle_request(const std::string& method, const std::string& uri,
 		return;
 	}
 
-	if (uri != "/") {
-		pipe_->send_response(HTTPProtocol::NotFound, "No such URI.");
+	if (uri[0] != '/') {
+		pipe_->send_response(HTTPProtocol::NotFound, "Invalid URI.");
 		return;
 	}
 
-	HTMLConfigExporter exporter;
+	std::vector<Buffer> path_components = Buffer(uri).split('/', false);
+	std::string select;
+	if (!path_components.empty()) {
+		if (path_components.size() != 1) {
+			pipe_->send_response(HTTPProtocol::NotFound, "Too many path components in URI.");
+			return;
+		}
+		path_components[0].extract(select);
+	}
+
+	HTMLConfigExporter exporter(select);
 	exporter.config(config_);
 	pipe_->send_response(HTTPProtocol::OK, "<html><head><title>WANProxy Configuration Monitor</title><style type=\"text/css\">body { font-family: sans-serif; } td, th { vertical-align: text-top; text-align: left; }</style></head><body>" + exporter.os_.str() + "</body></html>", "text/html");
 }
