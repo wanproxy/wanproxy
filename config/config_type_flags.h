5b33eda1f5895a3de4269991dad98cd26ccc0bc7
Juli Mallett <juli@clockworksquid.com>
2011-11-21 23:54:07 +0000

    Fix name collision.

--- config/config_type_flags.h
+++ config/config_type_flags.h
@@ -19,8 +19,8 @@ private:
 	std::map<const ConfigValue *, T> flags_;
 	std::map<std::string, T> flag_map_;
 public:
-	ConfigTypeFlags(const std::string& name, struct Mapping *mappings)
-	: ConfigType(name),
+	ConfigTypeFlags(const std::string& xname, struct Mapping *mappings)
+	: ConfigType(xname),
 	  flags_(),
 	  flag_map_()
 	{
