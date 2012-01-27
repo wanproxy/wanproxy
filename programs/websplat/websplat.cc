58fe19798b9290bd89f983a7bd79b76e1803a012
Juli Mallett <juli@clockworksquid.com>
2012-01-27 20:37:46 +0000

    Serve up files poorly.

--- programs/websplat/websplat.cc
+++ programs/websplat/websplat.cc
@@ -1,3 +1,7 @@
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
 #include <common/buffer.h>
 
 #include <event/action.h>
@@ -16,10 +20,20 @@
 
 #include <io/socket/simple_server.h>
 
+struct WebsplatConfig {
+	std::string root_;
+
+	WebsplatConfig(const std::string& root)
+	: root_(root)
+	{ }
+};
+
 class WebsplatClient : public HTTPServerHandler {
+	WebsplatConfig config_;
 public:
-	WebsplatClient(Socket *client)
-	: HTTPServerHandler(client)
+	WebsplatClient(const WebsplatConfig& config, Socket *client)
+	: HTTPServerHandler(client),
+	  config_(config)
 	{ }
 
 	~WebsplatClient()
@@ -59,7 +73,65 @@ private:
 		}
 		path_components.clear();
 
-		pipe_->send_response(HTTPProtocol::OK, "<html><head><title>Websplat - " + uri + "</title></head><body><h1>Welcome to Websplat!</h1><p>Have a nice " + method + "</p></body></html>", "text/html");
+		std::string path = config_.root_ + "/";
+		Buffer::join(path_stack, "/").extract(path);
+
+		handle_file(path);
+	}
+
+	void handle_file(const std::string& path)
+	{
+		DEBUG(log_) << "Attempting to send file: " << path;
+
+		int fd;
+		fd = open(path.c_str(), O_RDONLY);
+		if (fd == -1) {
+			/* XXX Check errno.  */
+			pipe_->send_response(HTTPProtocol::NotFound, std::string("open: error [") + strerror(errno) + "] for path: " + path);
+			return;
+		}
+
+		struct stat sb;
+		int rv = fstat(fd, &sb);
+		if (rv != 0) {
+			close(fd);
+			pipe_->send_response(HTTPProtocol::NotFound, std::string("fstat: error [") + strerror(errno) + "] for path: " + path);
+			return;
+		}
+
+		switch (sb.st_mode & S_IFMT) {
+		case S_IFREG:
+			break;
+		case S_IFDIR:
+			close(fd);
+			handle_file(path + "/index.html");
+			return;
+		default:
+			close(fd);
+			pipe_->send_response(HTTPProtocol::NotFound, "No file was found at that path, wink-wink, nudge-nudge.");
+			return;
+		}
+
+		Buffer data;
+		for (;;) {
+			uint8_t buf[65536];
+			ssize_t amt = read(fd, buf, sizeof buf);
+			if (amt == 0)
+				break;
+			if (amt == -1) {
+				close(fd);
+				pipe_->send_response(HTTPProtocol::NotFound, "Could not read file.");
+				return;
+			}
+			data.append(buf, (size_t)amt);
+		}
+		close(fd);
+
+		std::ostringstream os;
+		os << "Content-length: " << data.length() << "\r\n";
+		Buffer headers(os.str());
+
+		pipe_->send_response(HTTPProtocol::OK, &data, &headers);
 	}
 };
 
@@ -69,6 +141,7 @@ int
 main(int argc, char *argv[])
 {
 	std::string interface("");
+	std::string root("");
 	bool quiet, verbose;
 	int ch;
 
@@ -76,16 +149,19 @@ main(int argc, char *argv[])
 	verbose = false;
 
 	INFO("/websplat") << "Websplat";
-	INFO("/websplat") << "Copyright (c) 2008-2011 WANProxy.org.";
+	INFO("/websplat") << "Copyright (c) 2008-2012 WANProxy.org.";
 	INFO("/websplat") << "All rights reserved.";
 
-	while ((ch = getopt(argc, argv, "i:qv")) != -1) {
+	while ((ch = getopt(argc, argv, "i:qr:v")) != -1) {
 		switch (ch) {
 		case 'i':
 			interface = optarg;
 			break;
 		case 'q':
 			quiet = true;
+			break;
+		case 'r':
+			root = optarg;
 			break;
 		case 'v':
 			verbose = true;
@@ -95,7 +171,7 @@ main(int argc, char *argv[])
 		}
 	}
 
-	if (interface == "")
+	if (interface == "" || root == "")
 		usage();
 
 	if (quiet && verbose)
@@ -109,7 +185,7 @@ main(int argc, char *argv[])
 		Log::mask(".?", Log::Info);
 	}
 
-	new HTTPServer<TCPServer, WebsplatClient>(SocketAddressFamilyIP, interface);
+	new HTTPServer<TCPServer, WebsplatClient, WebsplatConfig>(WebsplatConfig(root), SocketAddressFamilyIP, interface);
 
 	event_main();
 }
@@ -117,6 +193,6 @@ main(int argc, char *argv[])
 static void
 usage(void)
 {
-	INFO("/websplat/usage") << "websplat [-q | -v] -i interface";
+	INFO("/websplat/usage") << "websplat [-q | -v] -i interface -r root";
 	exit(1);
 }
