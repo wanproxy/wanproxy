4f62c292c4b4163131b1d6de48d8faf560cdaab0
Juli Mallett <juli@clockworksquid.com>
2012-08-20 05:53:15 +0000

    Use name-list encoding.

--- ssh/ssh_algorithm_negotiation.cc
+++ ssh/ssh_algorithm_negotiation.cc
@@ -15,16 +15,14 @@ namespace {
 	};
 
 	template<typename T>
-	Buffer name_list(T list)
+	std::vector<Buffer> names(T list)
 	{
 		typename T::const_iterator it;
-		std::vector<Buffer> names;
-		Buffer out;
+		std::vector<Buffer> vec;
 
 		for (it = list.begin(); it != list.end(); ++it)
-			names.push_back(it->first);
-		SSH::NameList::encode(&out, names);
-		return (out);
+			vec.push_back(it->first);
+		return (vec);
 	}
 }
 
@@ -48,16 +46,16 @@ SSH::AlgorithmNegotiation::output(Buffer *out)
 
 	out->append(SSH::Message::KeyExchangeInitializationMessage);
 	out->append(constant_cookie, sizeof constant_cookie); /* XXX Synchronous random byte generation?  */
-	out->append(name_list(algorithms_.key_exchange_map_));
-	out->append(name_list(algorithms_.server_host_key_map_));
-	out->append(name_list(algorithms_.encryption_client_to_server_map_));
-	out->append(name_list(algorithms_.encryption_server_to_client_map_));
-	out->append(name_list(algorithms_.mac_client_to_server_map_));
-	out->append(name_list(algorithms_.mac_server_to_client_map_));
-	out->append(name_list(algorithms_.compression_client_to_server_map_));
-	out->append(name_list(algorithms_.compression_server_to_client_map_));
-	out->append(name_list(algorithms_.language_client_to_server_map_));
-	out->append(name_list(algorithms_.language_server_to_client_map_));
+	SSH::NameList::encode(out, names(algorithms_.key_exchange_map_));
+	SSH::NameList::encode(out, names(algorithms_.server_host_key_map_));
+	SSH::NameList::encode(out, names(algorithms_.encryption_client_to_server_map_));
+	SSH::NameList::encode(out, names(algorithms_.encryption_server_to_client_map_));
+	SSH::NameList::encode(out, names(algorithms_.mac_client_to_server_map_));
+	SSH::NameList::encode(out, names(algorithms_.mac_server_to_client_map_));
+	SSH::NameList::encode(out, names(algorithms_.compression_client_to_server_map_));
+	SSH::NameList::encode(out, names(algorithms_.compression_server_to_client_map_));
+	SSH::NameList::encode(out, names(algorithms_.language_client_to_server_map_));
+	SSH::NameList::encode(out, names(algorithms_.language_server_to_client_map_));
 	out->append(SSH::Boolean::False);
 	uint32_t reserved(0);
 	out->append(&reserved);
