8fc0417f4e7d1e7d38f08756e129a4df255af457
Juli Mallett <juli@clockworksquid.com>
2012-09-29 23:22:32 +0000

    Need to be able to truncate the digest, not the key.

--- ssh/ssh_mac.cc
+++ ssh/ssh_mac.cc
@@ -4,12 +4,14 @@ namespace {
 	struct ssh_mac_algorithm {
 		const char *rfc4250_name_;
 		CryptoMAC::Algorithm crypto_algorithm_;
-		unsigned key_size_;
+		unsigned size_;
 	};
 
 	static const struct ssh_mac_algorithm ssh_mac_algorithms[] = {
 		{ "hmac-md5",		CryptoMAC::MD5,		0 },
+		{ "hmac-md5-96",	CryptoMAC::MD5,		12 },
 		{ "hmac-sha1",		CryptoMAC::SHA1,	0 },
+		{ "hmac-sha1-96",	CryptoMAC::SHA1,	12 },
 		{ "hmac-sha256",	CryptoMAC::SHA256,	0 },
 		{ NULL,			CryptoMAC::MD5,		0 }
 	};
@@ -18,8 +20,8 @@ namespace {
 		LogHandle log_;
 		CryptoMAC::Instance *instance_;
 	public:
-		CryptoSSHMAC(const std::string& xname, CryptoMAC::Instance *instance, unsigned xkey_size)
-		: SSH::MAC(xname, instance->size(), xkey_size == 0 ? instance->size() : xkey_size),
+		CryptoSSHMAC(const std::string& xname, CryptoMAC::Instance *instance, unsigned xsize)
+		: SSH::MAC(xname, xsize == 0 ? instance->size() : xsize, instance->size()),
 		  log_("/ssh/mac/crypto/" + xname),
 		  instance_(instance)
 		{ }
@@ -62,7 +64,7 @@ SSH::MAC::algorithm(CryptoMAC::Algorithm xalgorithm)
 			ERROR("/ssh/mac") << "Could not get instance for algorithm: " << xalgorithm;
 			return (NULL);
 		}
-		return (new CryptoSSHMAC(alg->rfc4250_name_, instance, alg->key_size_));
+		return (new CryptoSSHMAC(alg->rfc4250_name_, instance, alg->size_));
 	}
 	ERROR("/ssh/mac") << "No SSH MAC support is available for algorithm: " << xalgorithm;
 	return (NULL);
