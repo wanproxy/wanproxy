a9c8c2c0ef3e8048cd9199b578d0cc186b2b9c45
Juli Mallett <juli@clockworksquid.com>
2012-12-27 08:20:40 +0000

    Add support for AES-128 in CTR mode.

--- crypto/crypto_encryption_openssl.cc
+++ crypto/crypto_encryption_openssl.cc
@@ -1,3 +1,4 @@
+#include <openssl/aes.h>
 #include <openssl/evp.h>
 
 #include <common/factory.h>
@@ -110,6 +111,105 @@ namespace {
 		}
 	};
 
+	class SessionAES128CTR : public CryptoEncryption::Session {
+		LogHandle log_;
+		AES_KEY key_;
+		uint8_t iv_[AES_BLOCK_SIZE];
+	public:
+		SessionAES128CTR(void)
+		: log_("/crypto/encryption/session/openssl"),
+		  key_(),
+		  iv_()
+		{ }
+
+		~SessionAES128CTR()
+		{ }
+
+		unsigned block_size(void) const
+		{
+			return (AES_BLOCK_SIZE);
+		}
+
+		unsigned key_size(void) const
+		{
+			return (AES_BLOCK_SIZE);
+		}
+
+		unsigned iv_size(void) const
+		{
+			return (AES_BLOCK_SIZE);
+		}
+
+		Session *clone(void) const
+		{
+			return (new SessionAES128CTR());
+		}
+
+		bool initialize(CryptoEncryption::Operation operation, const Buffer *key, const Buffer *iv)
+		{
+			(void)operation;
+
+			if (key->length() != AES_BLOCK_SIZE)
+				return (false);
+
+			if (iv->length() != AES_BLOCK_SIZE)
+				return (false);
+
+			uint8_t keydata[key->length()];
+			key->copyout(keydata, sizeof keydata);
+
+			AES_set_encrypt_key(keydata, AES_BLOCK_SIZE * 8, &key_);
+
+			iv->copyout(iv_, sizeof iv_);
+
+			return (true);
+		}
+
+		bool cipher(Buffer *out, const Buffer *in)
+		{
+			ASSERT(log_, in->length() % AES_BLOCK_SIZE == 0);
+
+			/*
+			 * Temporaries for AES_ctr128_encrypt.
+			 *
+			 * Their values only need to persist if we aren't using block-sized
+			 * buffers, which we are.  We could just use AES_ctr128_inc and do
+			 * the crypt operation by hand here.
+			 */
+			uint8_t counterbuf[AES_BLOCK_SIZE]; /* Will be initialized if countern==0.  */
+			unsigned countern = 0;
+
+			/*
+			 * We process a single, large, linear byte buffer here rather
+			 * than going a BufferSegment at a time, even though the byte
+			 * buffer is less efficient than some alternatives, because
+			 * there are padding and buffering implications if each
+			 * BufferSegment's length is not modular to the block size.
+			 */
+			uint8_t indata[in->length()];
+			in->copyout(indata, sizeof indata);
+
+			uint8_t outdata[sizeof indata];
+			AES_ctr128_encrypt(indata, outdata, sizeof indata, &key_, iv_, counterbuf, &countern);
+
+			out->append(outdata, sizeof outdata);
+			return (true);
+		}
+
+		Action *submit(Buffer *in, EventCallback *cb)
+		{
+			Buffer out;
+			if (!cipher(&out, in)) {
+				in->clear();
+				cb->param(Event::Error);
+				return (cb->schedule());
+			}
+			in->clear();
+			cb->param(Event(Event::Done, out));
+			return (cb->schedule());
+		}
+	};
+
 	class MethodOpenSSL : public CryptoEncryption::Method {
 		LogHandle log_;
 		FactoryMap<CryptoEncryption::Cipher, CryptoEncryption::Session> cipher_map_;
@@ -130,6 +230,9 @@ namespace {
 			cipher_map_.enter(CryptoEncryption::Cipher(CryptoEncryption::AES128, CryptoEncryption::CTR), evp_factory(EVP_aes_128_ctr()));
 			cipher_map_.enter(CryptoEncryption::Cipher(CryptoEncryption::AES192, CryptoEncryption::CTR), evp_factory(EVP_aes_192_ctr()));
 			cipher_map_.enter(CryptoEncryption::Cipher(CryptoEncryption::AES256, CryptoEncryption::CTR), evp_factory(EVP_aes_256_ctr()));
+#else
+			factory<SessionAES128CTR> aes128ctr_factory;
+			cipher_map_.enter(CryptoEncryption::Cipher(CryptoEncryption::AES128, CryptoEncryption::CTR), aes128ctr_factory());
 #endif
 #ifndef	OPENSSL_NO_BF
 			cipher_map_.enter(CryptoEncryption::Cipher(CryptoEncryption::Blowfish, CryptoEncryption::CBC), evp_factory(EVP_bf_cbc()));
