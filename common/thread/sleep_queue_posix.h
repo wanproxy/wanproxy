e19351ba6c69df368307059023b68d02b1f04e50
Juli Mallett <juli@clockworksquid.com>
2013-05-14 07:19:05 +0000

    Move SleepQueueState to a header, like for MutexState and ThreadState.

--- common/thread/sleep_queue_posix.h
+++ common/thread/sleep_queue_posix.h
@@ -23,44 +23,56 @@
  * SUCH DAMAGE.
  */
 
-#include <pthread.h>
+#ifndef	COMMON_THREAD_SLEEP_QUEUE_POSIX_H
+#define	COMMON_THREAD_SLEEP_QUEUE_POSIX_H
 
-#include <common/thread/mutex.h>
-#include <common/thread/sleep_queue.h>
-#include <common/thread/thread.h>
+#include "mutex_posix.h"
 
-#include "sleep_queue_posix.h"
+struct SleepQueueState {
+	pthread_cond_t cond_;
+	MutexState *mutex_state_;
 
-SleepQueue::SleepQueue(const std::string& name, Mutex *mutex)
-: name_(name),
-  mutex_(mutex),
-  state_(new SleepQueueState(mutex->state_))
-{ }
+	SleepQueueState(MutexState *mutex_state)
+	: cond_(),
+	  mutex_state_(mutex_state)
+	{
+		int rv;
 
-SleepQueue::~SleepQueue()
-{
-#if 0 /* XXX What about extern SleepQueues?  */
-	ASSERT_LOCK_OWNED(log_, mutex_);
-#endif
-
-	if (state_ != NULL) {
-		delete state_;
-		state_ = NULL;
+		rv = pthread_cond_init(&cond_, NULL);
+		ASSERT("/sleep/queue/posix/state", rv != -1);
 	}
-}
 
-void
-SleepQueue::signal(void)
-{
-	ASSERT_LOCK_OWNED("/sleep/queue", mutex_);
-	state_->signal();
-	ASSERT_LOCK_OWNED("/sleep/queue", mutex_);
-}
+	~SleepQueueState()
+	{
+		int rv;
 
-void
-SleepQueue::wait(void)
-{
-	ASSERT_LOCK_OWNED("/sleep/queue", mutex_);
-	state_->wait();
-	ASSERT_LOCK_OWNED("/sleep/queue", mutex_);
-}
+		rv = pthread_cond_destroy(&cond_);
+		ASSERT("/sleep/queue/posix/state", rv != -1);
+	}
+
+	void signal(void)
+	{
+		int rv;
+
+		mutex_state_->lock();
+		ASSERT("/sleep/queue/posix/state", mutex_state_->owner_ == Thread::self());
+		rv = pthread_cond_signal(&cond_);
+		ASSERT("/sleep/queue/posix/state", mutex_state_->owner_ == Thread::self());
+		mutex_state_->unlock();
+		ASSERT("/sleep/queue/posix/state", rv != -1);
+	}
+
+	void wait(void)
+	{
+		int rv;
+
+		mutex_state_->lock();
+		mutex_state_->lock_release();
+		rv = pthread_cond_wait(&cond_, &mutex_state_->mutex_);
+		ASSERT("/sleep/queue/posix/state", rv != -1);
+		mutex_state_->lock_acquire();
+		mutex_state_->unlock();
+	}
+};
+
+#endif /* !COMMON_THREAD_SLEEP_QUEUE_POSIX_H */
