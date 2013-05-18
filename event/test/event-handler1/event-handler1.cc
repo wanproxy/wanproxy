2dc14e083b52116dfaea76ac6f99257b50949396
Juli Mallett <juli@clockworksquid.com>
2013-05-18 07:20:01 +0000

    Update copyright years on files modified in this branch.

--- event/test/event-handler1/event-handler1.cc
+++ event/test/event-handler1/event-handler1.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Juli Mallett. All rights reserved.
+ * Copyright (c) 2012-2013 Juli Malett. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
