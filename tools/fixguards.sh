d06bb0cf23a126886477e2b54e7e92c5a2c50461
Juli Mallett <juli@clockworksquid.com>
2013-06-27 00:10:44 +0000

    Fix for addition of copyright block.  Relies on exact copyright length, sadly.  Should really look for existing guards?

--- tools/fixguards.sh
+++ tools/fixguards.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-files=`find . -name '*.h' | cut -d / -f 2- | xargs`
+files=`find . -name '*.h' | grep -v uinet | cut -d / -f 2- | xargs`
 
 for _file in ${files}; do
 	_guard=`echo ${_file} | tr a-z/. A-Z__`
-	( echo '#ifndef	'${_guard} ; echo '#define	'${_guard} ; ( cat ${_file} | tail -n +3 | sed '$d' ) ; echo '#endif /* !'${_guard}' */' ) > x
+	( head -25 ${_file} ; echo '#ifndef	'${_guard} ; echo '#define	'${_guard} ; ( cat ${_file} | tail -n +28 | sed '$d' ) ; echo '#endif /* !'${_guard}' */' ) > x
 	cat x > ${_file}
 	rm x
 done
